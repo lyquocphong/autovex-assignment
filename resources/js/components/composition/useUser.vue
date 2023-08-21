e7884dfbf47452cdd691d9ebcfe8ab1902be7277
Phong Ly <lyquocphong@gmail.com>
2023-08-21 08:35:40 +0300

    Edit hooks
    I have used wrong way to get state from pinia. Now i need to return the refs from store to make it reactive in UI

--- resources/js/components/composition/useUser.vue
+++
@@ -1,9 +0,0 @@
-<script>
-import { useAuthStore } from "../../store/auth";
-
-export default () => {
-  const authStore = useAuthStore();  
-    // expose to template and other options API hooks
-    return authStore.user;
-}
-</script>
