68756736f672116e602cb9a5513b83edeff2d187
Phong Ly <lyquocphong@gmail.com>
2023-08-21 08:41:48 +0300

    use useUser in right way
    Before useUser is implemented not return refs so that the ui is not reactive

--- resources/js/pages/DashboardPage.vue
+++ resources/js/pages/DashboardPage.vue
@@ -2,30 +2,33 @@
 import Clock from "../components/clock/Clock.vue";
 import ClockView from "../components/clock/ClockView.vue";
 import ClockEditForm from "../components/clock/forms/ClockEditForm.vue";
-import useUser from "../components/composition/useUser.vue";
-import useSetting from "../components/composition/useSetting.vue";
+import useUser from "../components/composition/useUser";
+import useSetting from "../components/composition/useSetting";
 
-const { name } = useUser();
-const { clock } = useSetting();
+const user = useUser();
+const setting = useSetting();
 </script>
 
 <template>
-  <h2 class="text-center">Welcome back {{ name }}</h2>
-  <Clock>
-    <template #default="defaultProps">
-      <ClockView
-        :hour="clock.hour"
-        :minute="clock.minute"
-        :setIsEditing="defaultProps.setIsEditing"
-      />
-    </template>
+  <main v-if="user">
+    <h2 class="text-center">Welcome back {{ user.name }}</h2>
+    <Clock>
+      <template #default="defaultProps">
+        <ClockView
+          v-if="setting.clock"
+          :hour="setting.clock.hour"
+          :minute="setting.clock.minute"
+          :setIsEditing="defaultProps.setIsEditing"
+        />
+      </template>
 
-    <template #editForm="editFormProps">
-      <ClockEditForm
-        :hour="clock.hour"
-        :minute="clock.minute"
-        :setIsEditing="editFormProps.setIsEditing"
-      />
-    </template>
-  </Clock>
+      <template #editForm="editFormProps">
+        <ClockEditForm
+          :hour="setting.clock.hour"
+          :minute="setting.clock.minute"
+          :setIsEditing="editFormProps.setIsEditing"
+        />
+      </template>
+    </Clock>
+  </main>
 </template>
